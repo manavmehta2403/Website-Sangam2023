4bee3e9518866c8185fdb05db773aaf1c6bf7e4c
Manav Mehta <manav.mehta@wuh-group.com>
2022-12-07 17:06:54 +0100

    UPDATED 2

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// add the aeroplane scroll
+//join links in navbar
+console.log("connected");
+//dynamically set the height of the progress bar
+var progressBar = document.querySelector(".progress-container");
+var BenefitsSection = document.querySelector(".benefits");
+
+console.log(BenefitsSection.style);
+
+progressBar.style.height =  BenefitsSection.style.height;
